252f7b93642044f9ab24e2c8cc61689889a759c5
rsudo <badbad7351@gmail.com>
2021-08-09 17:43:15 +0900

    first commit

---
+++ test/main.c
@@ -0,0 +1,26 @@
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//-lreadline
+int main(void)
+{
+	char *command;
+
+	using_history();
+	read_history(".my_history"); // [ToDo]historyファイルが無いときの動作の検証
+	while (1)
+	{
+		command = readline(">> ");
+		// printf("command: %s\n", command);
+		add_history(command);
+
+		/* コマンドの処理 */
+
+		free(command);
+	}
+	write_history(".my_history");
+
+	return 0;
+}
