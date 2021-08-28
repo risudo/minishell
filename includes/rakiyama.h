44d592a0f537718a345548f801db7a7022d0c223
rakiyama <ryojiro25@gmail.com>
2021-08-28 23:59:35 +0900

    mod xdup xdup2

--- includes/rakiyama.h
+++ includes/rakiyama.h
@@ -12,11 +12,14 @@
 # include <errno.h>
 # include "minishell.h"
 # include "parse.h"
+# include <readline/readline.h>
+# include <readline/history.h>
 
 char	*ft_xstrjoin(char *str1, char *str2);
 char	**ft_xsplit(char *src_str, char cut_char);
 void	xclose(int fd);
-void	xdup2(int oldfd, int newfd);
+int		xdup2(int oldfd, int newfd);
+int		xdup(int oldfd);
 void	xwaitpid(pid_t pid, int *wstatus, int options);
 void	xpipe(int *pipefd);
 pid_t	xfork(void);
--- srcs/wrapper2.c
+++ srcs/wrapper2.c
@@ -36,14 +36,32 @@ void	xclose(int fd)
 	}
 }
 
-void	xdup2(int oldfd, int newfd)
+int	xdup2(int oldfd, int newfd)
 {
-	if (dup2(oldfd, newfd) == -1)
+	int	fd;
+
+	fd = dup2(oldfd, newfd);
+	if (fd == -1)
 	{
 		ft_putstr_fd("dup2 : ", STDERR_FILENO);
 		ft_putendl_fd(strerror(errno), STDERR_FILENO);
 		exit(EXIT_FAILURE);
 	}
+	return (fd);
+}
+
+int	xdup(int oldfd)
+{
+	int	newfd;
+
+	newfd = dup(oldfd);
+	if (newfd == -1)
+	{
+		ft_putstr_fd("dup : ", STDERR_FILENO);
+		ft_putendl_fd(strerror(errno), STDERR_FILENO);
+		exit(EXIT_FAILURE);
+	}
+	return (newfd);
 }
 
 void	xwaitpid(pid_t pid, int *wstatus, int options)
