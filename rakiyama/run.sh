57b9afbab5cfcda3f16d4c32e233b6c777912a18
rakiyama <ryojiro25@gmail.com>
2021-08-29 21:03:34 +0900

    delete previous rakiyama/*.c mod run.sh

--- rakiyama/run.sh
+++ rakiyama/run.sh
@@ -1,3 +1,3 @@
 make -C ../libft
-gcc -lreadline -lhistory -L/usr/local/opt/readline/lib -I/usr/local/opt/readline/include -I ../includes/ *.c ../srcs/wrapper1.c ../srcs/wrapper2.c ../libft/libft.a
+gcc -lreadline -lhistory -L/usr/local/opt/readline/lib -I/usr/local/opt/readline/include -I ../includes/ ../srcs/*.c for_running_and_utils.c ../libft/libft.a
 #./a.out
