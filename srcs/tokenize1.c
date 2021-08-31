96b9722b9f29f9352a7fe880b3306dceeaa7dbfd
rsudo <badbad7351@gmail.com>
2021-08-31 17:17:41 +0900

    fix: tokenize

--- srcs/tokenize1.c
+++ srcs/tokenize1.c
@@ -73,7 +73,7 @@ t_token	*tokenize_cmd_by_space(char *cmd, unsigned char *status, bool *err)
 	while (*cmd)
 	{
 		if (ft_isspace(*cmd) && !(flag_quot == D_QUOT || flag_quot == S_QUOT))
-			cur = new_token(cur, &cmd, &start, &flag_quot);
+			cur = new_token(cur, &cmd, &start, &flag_quot), cmd--;
 		flag_quot = get_flag_quot(cmd, flag_quot), cmd++;
 	}
 	if (cur != NULL && cmd != start)
