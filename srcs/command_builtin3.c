3317b804cb21a40550cea8103d9a64065c1dc123
rakiyama <ryojiro25@gmail.com>
2021-09-18 23:47:36 +0900

    mod exit

--- srcs/command_builtin3.c
+++ srcs/command_builtin3.c
@@ -39,20 +39,21 @@ void	builtin_exit(t_execdata *data)
 	bool	nonnum_check;
 
 	ft_putendl_fd("exit", STDERR_FILENO);
-	if (data->cmdline[1] == NULL)
-		exit(g_status);
-	if (data->cmdline[2])
+	if (data->cmdline[1])
 	{
-		ft_puterror("exit", "too many arguments", NULL);
-		g_status = 2;
-		return ;
-	}
-	g_status = (unsigned char)ft_atol(data->cmdline[1], &nonnum_check);
-	if (nonnum_check)
-	{
-		ft_puterror("exit", data->cmdline[1], \
-						"numeric argument required");
-		g_status = 2;
+		g_status = (unsigned char)ft_atol(data->cmdline[1], &nonnum_check);
+		if (nonnum_check)
+		{
+			ft_puterror("exit", data->cmdline[1], \
+							"numeric argument required");
+			g_status = 2;
+		}
+		else if (data->cmdline[2])
+		{
+			ft_puterror("exit", "too many arguments", NULL);
+			g_status = 1;
+			return ;
+		}
 	}
 	exit(g_status);
 }
